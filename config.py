5076e149ac39f9509f4facbc999c50bd458cca4a
Tyoma Kazakov <tyoma@robotlovesyou.ru>
2013-10-31 01:26:00 +0400

    prod db URI

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 -*-
 import os
 
-DATABASE_NAME = "commute_time"
+DATABASE_NAME = "heroku_app18764095"
 
-MONGODB_URI = "mongodb://localhost:27017/" + DATABASE_NAME
+#MONGODB_URI = "mongodb://localhost:27017/" + DATABASE_NAME
+
+MONGODB_URI = "mongodb://heroku:8Gn-Cpc-Nv3-uuD@ds053128.mongolab.com:53128/heroku_app18764095"
 MONGODB_URI = os.getenv('MONGOLAB_URI', MONGODB_URI)
