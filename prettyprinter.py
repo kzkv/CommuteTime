0989b15bd96650de4cbbb42b7a6cdd4ced7c7323
Tyoma Kazakov <tyoma@robotlovesyou.ru>
2013-11-08 11:48:21 +0400

    pretty printer upgrade

--- prettyprinter.py
+++ prettyprinter.py
@@ -1,8 +1,16 @@
 # -*- coding: UTF-8 -*-
 import pprint
 
-class MyPrettyPrinter(pprint.PrettyPrinter):
+
+class _MyPrettyPrinter(pprint.PrettyPrinter):
     def format(self, object, context, maxlevels, level):
         if isinstance(object, unicode):
             return (object.encode('utf8'), True, False)
         return pprint.PrettyPrinter.format(self, object, context, maxlevels, level)
+
+
+_pprinter = _MyPrettyPrinter()
+
+
+def pretty_print(obj):
+    _pprinter.pprint(obj)
