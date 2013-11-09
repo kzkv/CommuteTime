34c51d1d7beb810f7a91105165712554022dc2f4
Tyoma Kazakov <tyoma@robotlovesyou.ru>
2013-11-09 15:59:20 +0400

    db use fix

--- config.py
+++ config.py
@@ -3,9 +3,6 @@ import os
 
 DATABASE_NAME = "heroku_app18764095"
 
-#MONGODB_URI = "mongodb://localhost:27017/" + DATABASE_NAME
-
-MONGODB_URI = "mongodb://heroku:8Gn-Cpc-Nv3-uuD@ds053128.mongolab.com:53128/heroku_app18764095"
-MONGODB_URI = os.getenv('MONGOLAB_URI', MONGODB_URI)
+MONGODB_URI = os.getenv('MONGOLAB_URI')
 
 AWS_STORAGE_BUCKET_NAME = "commutetime"
